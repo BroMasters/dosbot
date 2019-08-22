4a37b6c007fdbaf9486a0c166497c6e823dd9c54
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 19:26:06 +0300

    Update index.py

--- index.py
+++ index.py
@@ -155,6 +155,6 @@ while True:
      sock.sendto(bytes, (ip,port));
      sent = sent + 1;
      port = port + 1;
-     print "\033[0;32;47m%s (%s:%s)" %(sent,ip,port);
+     print "\033[1;36;47m%packet (%ip:%port)" %(sent,ip,port);
      if port == 65534:
        port = 1;
