04705d0f1e8fc76f74231174944a2b2fd12e1293
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 17:00:38 +0300

    Update index.py

--- index.py
+++ index.py
@@ -40,6 +40,6 @@ while True:
     sock.sendto(bytes, (ip,port));
     sent = sent + 1;
     port = port + 1;
-    print "\033[92m%s. paket, %s adresine; %s portuyla gönderildi"%(sent,ip,port);
+    print "\033[92m%s. paket, %s adresine %s portuyla gönderildi"%(sent,ip,port);
     if port == 65534:
       port = 1;
